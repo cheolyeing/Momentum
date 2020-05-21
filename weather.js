870f5b5e112726db9eae20a05d074b3e82f7a7d6
임희철\hp <cheolyeing@gmail.com>
2020-05-21 16:10:26 +0900

    Finish without weather API

---
+++ weather.js
@@ -0,0 +1,44 @@
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?${lat}=35&${lng}=125`);
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude: latitude,
+    longitude: longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Can't Access");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
